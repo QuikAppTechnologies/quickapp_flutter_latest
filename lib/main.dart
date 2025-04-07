94623d0f99c76e05f4f6b9f08221a6d8f7bcb240
QuikAppTech <110347968+QuikAppTech@users.noreply.github.com>
2025-04-08 00:13:39 +0530

    Push Notify updated

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'firebase_initializer.dart';
+// import 'firebase_initializer.dart';
 
 bool hasInternet = true;
 
