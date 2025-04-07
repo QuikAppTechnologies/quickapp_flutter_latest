267e9406b4ebe8fa337a6a27e5661a31f5a4cc76
QuikAppTech <110347968+QuikAppTech@users.noreply.github.com>
2025-04-07 23:40:19 +0530

    FirebaseInitializer changed

--- lib/firebase_initializer.dart
+++ lib/firebase_initializer.dart
@@ -1,42 +1,20 @@
-import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
-// class FirebaseInitializer {
-//   static Future<void> initialize() async {
-//     final pushNotify = Platform.environment['PUSH_NOTIFY'] == 'true';
-//     if (pushNotify) {
-//       await Firebase.initializeApp();
-//       FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-//       // Request permissions if needed (for iOS)
-//       if (!kIsWeb && (Platform.isIOS || Platform.isMacOS)) {
-//         await messaging.requestPermission();
-//       }
-
-//       String? token = await messaging.getToken();
-//       print('🔔 FCM Token: $token');
-//     } else {
-//       print('🚫 Push Notification not enabled');
-//     }
-//   }
-// }
 class FirebaseInitializer {
-  static bool get isEnabled =>
-      bool.fromEnvironment('PUSH_NOTIFY', defaultValue: false);
-
   static Future<void> initialize() async {
-    if (isEnabled) {
+    const bool firebaseEnabled =
+        bool.fromEnvironment('FIREBASE_ENABLED', defaultValue: false);
+
+    if (firebaseEnabled && !kIsWeb) {
       try {
         await Firebase.initializeApp();
-        print("✅ Firebase initialized.");
+        debugPrint('✅ Firebase initialized.');
       } catch (e) {
-        print("❌ Firebase init failed: $e");
+        debugPrint('⚠️ Firebase initialization failed: $e');
       }
     } else {
-      print("🚫 Firebase not enabled via PUSH_NOTIFY.");
+      debugPrint('🚫 Firebase not enabled via PUSH_NOTIFY.');
     }
   }
 }
